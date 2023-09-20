d914d45416b996a59d247345ffc999bc9a3776d6
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2023-09-20 15:49:53 +0100

    breakout digital; animation now broken!

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-// M E T E R S  -  Dials, Bars and DIGITALs 
+// M E T E R S  
 enum STYLES {
     //% block="Blob"
     BLOB,
@@ -11,58 +11,18 @@ enum STYLES {
     //% block="Needle"
     NEEDLE,
     //% block="Tidal"
-    TIDAL,
-    //% block="Digital" 
-    DIGITAL
+    TIDAL
 }
-/* all frames are coded column-wise from top-left as 25-bit bit-maps:
-*/
-const dialMaps = [
-    0x0001C00, 0x0009800, 0x0019000, 0x0111000, 0x0211000, 0x0221000,
-    0x0421000, 0x0821000, 0x0841000, 0x1041000, 0x00C1000, 0x0083000,
-    0x0007000, 0x0003200, 0x0001300, 0x0001110, 0x0001108, 0x0001088,
-    0x0001084, 0x0001082, 0x0001042, 0x0001041, 0x0001060, 0x0001820];
-const dialBound = 23;
 
-const blobMaps = [
-    0x0001000, 0x0023880, 0x00739C0, 0x0477DC4,
-    0x06F7DEC, 0x0EFFFEE, 0x1FFFFFF];
-const blobBound = 6;
-
-const barMaps = [
-    0x0004000, 0x0084200, 0x1084210, 0x1086210, 0x10C6310, 0x18C6318,
-    0x18C7318, 0x18E7398, 0x1CE739C, 0x1CE7B9C, 0x1CF7BDC, 0x1EF7BDE,
-    0x1EF7FDE, 0x1EFFFFE, 0x1FFFFFF];
-const barBound = 14;
-
-
-const spiralMaps = [
-    0x0001000, 0x0021000, 0x0061000, 0x0063000, 0x0063100, 0x00631C0, 0x00631C0, 0x00639C0,
-    0x00739C0, 0x02739C0, 0x06739C0, 0x0E739C0, 0x1E739C0, 0x1EF39C0,
-    0x1EF79C0, 0x1EF7BC0, 0x1EF7BD0, 0x1EF7BD8, 0x1EF7BDC, 0x1EF7BDC,
-    0x1EF7BDE, 0x1EF7BDF, 0x1EF7BFF, 0x1EF7FFF, 0x1EFFFFF, 0x1FFFFFF];
-const spiralBound = 25;
-
-
-
-const tidalMaps = [
-    0x0000010, 0x0000210, 0x0000218, 0x000021C, 0x00031C,
-    0x000431C, 0x008431C, 0x008631C, 0x008639C, 0x008639C,
-    0x008639E, 0x008639F, 0x00863DF, 0x00873DF, 0x00C73DF,
-    0x10C73DF, 0x18C73DF, 0x18E73DF, 0x18E7BDF, 0x18E7BFF,
-    0x18E7FFF, 0x18F7FFF, 0x1CF7FFF, 0x1EF7FFF, 0x1EFFFFF,
-    0x1FFFFFF];
-const tidalBound = 25;
-/* DIGITAL - 2-digit readout from 0 to 99
-
-    Each digit uses a 10-bit bitmap (0...1023) encoding pixels vertically 
-    from 2 adjacent display columns, either {0,1} or{3,4}:  
-        2^0=1   2^5=32 
-        2^1=2   2^6=64 
-        2^2=4   2^7=128 
-        2^4=8   2^8=256 
-        2^5=16  2^9=512 
-    the full 2-digit map is tensMap + (unitsMap << 15) 
+/* Default style is DIGITAL: a 2-digit readout from 0 to 99
+    Each digit uses a 10-bit bitmap (0...1023) encoding pixels vertically
+    from 2 adjacent display columns, either {0,1} or{3,4}:
+        2^0=1   2^5=32
+        2^1=2   2^6=64
+        2^2=4   2^7=128
+        2^4=8   2^8=256
+        2^5=16  2^9=512
+    combined as ((tensMap << 15) + unitsMap) to give a full bitmap 
 */
 const digitMaps = [
     0x1CE,  // 462,  //"0" 
@@ -76,14 +36,49 @@ const digitMaps = [
     0x37B,  // 891,  //"8" 
     0x3E7]; // 999   //"9" 
 const digitBound = 99;
+const digitStyle = -1;
 
-/*   NEEDLE - 17 positions showing 90-degree sweep from a corner
-Each 8-digit value ABCDEFGH encodes coords of four pixels:
-pixel1(A,B); pixel2(C,D); pixel3(E,F); pixel4(G,H)
-in addition to pixel(0,0), which is always on.
-
-Now re-coded as full 25-bit bit-maps:
+/* displayed frames are coded column-wise from top-left as 25-bit bit-maps:
 */
+// DIAL: 24 frames rotate 3-pixel pointer about middle
+const dialMaps = [
+    0x0001C00, 0x0009800, 0x0019000, 0x0111000, 0x0211000, 0x0221000,
+    0x0421000, 0x0821000, 0x0841000, 0x1041000, 0x00C1000, 0x0083000,
+    0x0007000, 0x0003200, 0x0001300, 0x0001110, 0x0001108, 0x0001088,
+    0x0001084, 0x0001082, 0x0001042, 0x0001041, 0x0001060, 0x0001820];
+const dialBound = 23;
+
+// BLOB: 7 frames growing outwards from middle
+const blobMaps = [
+    0x0001000, 0x0023880, 0x00739C0, 0x0477DC4,
+    0x06F7DEC, 0x0EFFFEE, 0x1FFFFFF];
+const blobBound = 6;
+
+// BAR: 15 frames of vertical bar-graph  
+const barMaps = [
+    0x0004000, 0x0084200, 0x1084210, 0x1086210, 0x10C6310, 0x18C6318,
+    0x18C7318, 0x18E7398, 0x1CE739C, 0x1CE7B9C, 0x1CF7BDC, 0x1EF7BDE,
+    0x1EF7FDE, 0x1EFFFFE, 0x1FFFFFF];
+const barBound = 14;
+
+// SPIRAL: 25 frames winding outwards from middle
+const spiralMaps = [
+    0x0001000, 0x0021000, 0x0061000, 0x0063000, 0x0063100, 0x00631C0, 
+    0x00631C0, 0x00639C0, 0x00739C0, 0x02739C0, 0x06739C0, 0x0E739C0, 
+    0x1E739C0, 0x1EF79C0, 0x1EF7BC0, 0x1EF7BD0, 0x1EF7BD8, 0x1EF7BDC, 
+    0x1EF7BDC, 0x1EF7BDE, 0x1EF7BDF, 0x1EF7BFF, 0x1EF7FFF, 0x1EFFFFF, 0x1FFFFFF];
+const spiralBound = 25;
+
+// TIDAL: 25 frames washing diagonally up from bottom-left corner
+const tidalMaps = [
+    0x0000010, 0x0000210, 0x0000218, 0x000021C, 0x000031C, 0x000431C, 
+    0x008431C, 0x008631C, 0x008639C, 0x008639E, 0x008639F, 0x00863DF, 
+    0x00873DF, 0x00C73DF, 0x10C73DF, 0x18C73DF, 0x18E73DF, 0x18E7BDF,  
+    0x18E7BFF, 0x18E7FFF, 0x18F7FFF, 0x1CF7FFF, 0x1EF7FFF, 0x1EFFFFF,
+    0x1FFFFFF];
+const tidalBound = 25;
+
+// NEEDLE: 17 frames swinging clockwise around top-left corner
 const needleMaps = [
     0x0108421,  //  0 ----
     0x0208421,  //  1 -
@@ -119,43 +114,34 @@ const blobBound = 7;
 
 //% color=#402080 weight=100 icon="\uf118" block="Meters" 
 namespace Meter {
-    let styleIs: number = STYLES.DIAL;
-    let mapSet: number[] = dialMaps;
-    let bound: number = dialBound; // number of displayable "frames" for ths style
+    let styleIs: number = digitStyle;
+    let mapSet: number[] = digitMaps; // array of frame bit-maps
+    let bound: number = digitBound;   // highest frame-index
     let fromValue: number = 0;     // the user's start value
     let uptoValue: number = 0;     // the user's end $value
     let valueNow: number = 0;      // the user's latest value
-    /* We save bit-maps of the pixels currently lit, and needing to be lit:
+
+    /* We save the bit-maps of the pixels currently lit, and needing to be lit:
       Computing (litMap XOR newMap) then shows which pixels will need to be toggled.
       Bits are allocated column-wise top-to-bottom and left-to-right,
       so pixel[x,y] contributes 2**(x*5 + y) to the 25-bit map.
-      (This matches the 2-column encoding used for each digit in STYLE.DIGITAL)
+      (This matches the 2-column encoding used for each digit in digitStyle)
     */
     let litMap: number = 0;
     let newMap: number = 0;
     let litFrame: number = -1; // currently displayed frame (-1 says none)
     let rangeFixed = false;    // notify overflow/underflow
     let rangeError = false;    // finalFrame was out of range before correction
+
 // declare some background variables...
-    let animating = false; // if true, animate intermediate frames
-    let tick = 0;          // target animation counting interval
+    let animating = false; // true while animating intermediate frames
+    let flashing = false;  // true while indicating frameError
     let firstFrame = 0;    // animation start-value
     let finalFrame = 0;    // animation end-value
     let when = 0;          // animation starting time
     let then = 0;          // animation target end time
-
-    // toggle the state of all pixels indicated in the 25-bit column-map: toToggle
-    function toggleColumnMap(toToggle: number) {
-        let bitmap = toToggle;
-        for (let x = 0; x < 5; x++) { // column-wise from top-left
-            for (let y = 0; y < 5; y++) {
-                if (bitmap & 1) {
-                    led.toggle(x, y);
-                }
-                bitmap >>= 1;
-            }
-        }
-    }
+    let tick = 0;          // animation counting interval
+    let flashGap = 125;    // flashing around 4 times/sec
 
     function mapToFrame(value: number, start: number, end: number, 
                         startFrame: number, endFrame: number): number {
@@ -189,7 +175,7 @@ namespace Meter {
     // update display to show new frame
     // (frame will always be in the range [0..bound])
     function showFrame(frame: number) {
-        if (styleIs == STYLES.DIGITAL) {
+        if (styleIs == digitStyle) {
             let tens = ~~(frame / 10);
             let units = frame % 10;
             // left-shift "units" map by 3 columns over to right side (=15 bits)
@@ -208,9 +194,16 @@ namespace Meter {
             toggleColumnMap(litMap); // start flashing immediately
         }
     }
+
+    function clearFrame() {
+        basic.clearScreen();
+        litMap = 0;
+        litFrame = -1;
+    }
+
     function flashFrame():void {
-        while (rangeError) {
-            basic.pause(200);  // flash about five times a second
+        while (flashing) {
+            basic.pause(flashGap);
             if (litMap != 0) {
                 clearFrame();
             } else {
@@ -219,29 +212,32 @@ namespace Meter {
         }
     }
 
-    function clearFrame() {
-        basic.clearScreen();
-        litMap = 0;
-        litFrame = -1;
+
+    // toggle the state of all pixels indicated in the 25-bit column-map: toToggle
+    function toggleColumnMap(toToggle: number) {
+        let bitmap = toToggle;
+        for (let x = 0; x < 5; x++) { // column-wise from top-left
+            for (let y = 0; y < 5; y++) {
+                if (bitmap & 1) {
+                    led.toggle(x, y);
+                }
+                bitmap >>= 1;
+            }
+        }
     }
 
     // EXPOSED USER INTERFACES  
 
-    //% block="Use %choice Meter to indicate values from $start to $limit" 
+    //% block="Use %choice Meter to show values from $start to $limit" 
     //% weight=20 
     export function use(style: STYLES, start: number, limit: number) {
         styleIs = style;
         fromValue = start;
         uptoValue = limit;
-        // adopt DIAL set as default for any as yet unimplemented STYLES
-        mapSet = dialMaps;
-        bound = dialBound;
         switch (style) {
             case STYLES.DIAL:
-                break;
-            case STYLES.DIGITAL:
-                mapSet = digitMaps; // NOTE: only 10 frames!
-                bound = digitBound; // ...combine to allow 100 values
+                mapSet = dialMaps;
+                bound = dialBound;
                 break;
             case STYLES.NEEDLE:
                 mapSet = needleMaps;
@@ -265,8 +261,22 @@ namespace Meter {
                 break;
         }
         rangeFixed = false;
+        rangeError = false;
         animating = false;
         styleIs = style;
+        clearFrame();
+    }
+
+    //% block="Use digital meter to indicate values from 0 to 99" 
+    //% weight=20 
+    export function digital() {
+        styleIs = digitStyle;
+        fromValue = 0;
+        uptoValue = 99; 
+        mapSet = digitMaps; // NOTE: the 10 numeric frames!
+        bound = digitBound; // ...combine to allow 100 values
+        rangeFixed = false;
+        animating = false;
         clearFrame();
     }
 
@@ -333,7 +343,6 @@ namespace Meter {
         show(fromValue);
     }
 }
-/****
 Meter.use(STYLES.BLOB, 0, 99);
 for (let i = 0; i < 100; i++) {
     Meter.show(i);
@@ -376,13 +385,14 @@ for (let i = 0; i < 100; i++) {
 }
 basic.pause(1000);
 
-Meter.use(STYLES.DIGITAL, 0, 99);
+Meter.digital();
 for (let i = 0; i < 100; i++) {
     Meter.show(i);
     basic.pause(200);
 }
 basic.pause(1000);
-***/
+
+/*******/
 
 Meter.use(STYLES.DIAL, 0, 99);
 basic.pause(1000);
